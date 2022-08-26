096fa578c21e3cf3d798c14b37c8f8b6d8800af5
Oliver Ocsai <oliver.ocsai@capgemini.com>
2022-08-26 15:28:26 +0200

    Initial commit

---
+++ ScpLogger/Logger.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using WinSCP;
+
+namespace ScpLogger
+{
+    public class Logger
+    {
+        public static string FileName = $"Log{DateTime.UtcNow.ToString("yyyy-dd-M--HH-mm-ss")}.txt";
+
+        public static string HostName { get; set; }
+        public static string LocalPath { get; set; }
+        public static string RemotePath { get; set; }
+        public static string UserName { get; set; }
+        public static string Password { get; set; }
+        public static int PortNumber { get; set; }
+
+        private static List<string> LogSum = new List<string>();
+        private static string GetExternalIp()
+        {
+
+            string externalIpString = new WebClient().DownloadString("http://icanhazip.com").Replace("\\r\\n", "").Replace("\\n", "").Trim();
+            var externalIp = IPAddress.Parse(externalIpString);
+
+            return externalIp.ToString();
+        }
+
+        public static void UploadLog()
+        {
+            ScpUploader uploader = new ScpUploader(HostName,UserName,Password,PortNumber,true);
+            File.WriteAllLines(FileName, LogSum);
+            uploader.Upload(LocalPath,RemotePath);
+        }
+
+        public static string Info(string message)
+        {
+            string result =
+                $"{DateTime.UtcNow}||{Environment.MachineName}||{GetExternalIp()}||{nameof(Info)}|| {message}";
+            LogSum.Add(result);
+            return result;
+        }
+
+        public static string Warning(string message)
+        {
+            string result =
+                $"{DateTime.UtcNow}||{Environment.MachineName}||{GetExternalIp()}||{nameof(Warning)}|| {message}";
+            LogSum.Add(result);
+            return result;
+        }
+
+        public static string Error(string message)
+        {
+            string result =
+                $"{DateTime.UtcNow}||{Environment.MachineName}||{GetExternalIp()}||{nameof(Error)}|| {message}";
+            LogSum.Add(result);
+            return result;
+        }
+    }
+}
---
+++ ScpLogger/ScpUploader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using WinSCP;
+
+namespace ScpLogger
+{
+    internal class ScpUploader
+    {
+        private static string HostName { get; set; }
+        private static string UserName { get; set; }
+        private static string Password { get; set; }
+        private static int PortNumber { get; set; }
+        private static bool GiveUpSecurity { get; set; }
+
+        public ScpUploader(string hostName,string userName,string password,int portNumber,bool giveUpSecurity)
+        {
+            HostName= hostName;
+            UserName = userName;
+            Password = password;
+            PortNumber = portNumber;
+            GiveUpSecurity = giveUpSecurity;
+        }
+
+        public void Upload(string localPath,string remotePath)
+        {
+
+            SessionOptions sessionOptions = new SessionOptions
+            {
+                Protocol = Protocol.Scp,
+                HostName = HostName,
+                UserName = UserName,
+                Password = Password,
+                PortNumber = PortNumber,
+                GiveUpSecurityAndAcceptAnySshHostKey = GiveUpSecurity
+            };
+
+            using (Session session = new Session())
+            {
+                // Connect
+                session.Open(sessionOptions);
+
+                // Upload files
+                TransferOptions transferOptions = new TransferOptions();
+                transferOptions.TransferMode = TransferMode.Binary;
+
+                TransferOperationResult transferResult;
+                transferResult =
+                    session.PutFiles(localPath, remotePath, false, transferOptions);
+
+                // Throw on any error
+                transferResult.Check();
+
+                // Print results
+                foreach (TransferEventArgs transfer in transferResult.Transfers)
+                {
+                    Console.WriteLine("Upload of {0} succeeded", transfer.FileName);
+                }
+            }
+        }
+    }
+}
